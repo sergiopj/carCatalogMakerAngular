c060fa7da9ec2b7ca1d84e9db88d048eebeafecb
Tu Nombre <you@example.com>
2018-08-07 18:33:44 +0200

    add login route

--- catalog-app/src/app/app.routing.ts
+++ catalog-app/src/app/app.routing.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { SelectElementsComponent } from './components/select-elements/select-elements.component';
+import { LoginComponent } from './components/login/login.component';
 
 // routes config
 const APP_ROUTES: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent }, // public
     { path: 'contact', component: ContactComponent }, // public
     { path: 'select-elements', component: SelectElementsComponent }, // private, need auth
