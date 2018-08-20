a448e9e3cc3caf9bb8bfa0ee72e75f9a5f106ac2
spj <sergiopjaranda@gmail.com>
2018-08-20 19:59:23 +0200

    test

--- catalog-app/src/app/components/select-elements/select-elements.component.ts
+++ catalog-app/src/app/components/select-elements/select-elements.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 // services
 import { AuthService } from '../../services/auth.service';
 import { UtilsService } from '../../services/utils.service';
@@ -11,16 +11,19 @@ import { timeout } from 'q';
   styleUrls: ['./select-elements.component.css']
 })
 // tslint:disable-next-line
-export class SelectElementsComponent implements OnInit {
+export class SelectElementsComponent implements OnInit { 
 
+  
+  
   public loginStatus: boolean;
   private currentDate: string;
   private selectedCarList = [];
-
+  private finalPrice: number;
 
   constructor( private _authService: AuthService,
                private _utilsService: UtilsService,
-               private _carDataService: CarDataService ) {
+               private _carDataService: CarDataService,
+               private changeDR: ChangeDetectorRef ) {
 
   }
 
@@ -33,24 +36,29 @@ export class SelectElementsComponent implements OnInit {
     this._carDataService.getCarData().catch( error => {
       throw new Error('Get car data failed');
     });
-
-    // use some equal to this to print in table car, find method search element in array or json
-    // let employeeDB = employees.find(employee => employee.id === id);
-    // fix to async and await here
-
   }
+
   // add car data to table
   addCarTable(carModelData) {
     this.selectedCarList.push(carModelData);
+    this.changeDR.detectChanges();
   }
 
-  onChangeRate(event) {
-
+  onChangeCalc(event) {
     // to apply discount to the price
-  
-    console.log(event);
-    // get price input value
-    // const carPrice = 
+    // cada vez que se use esta funcion se recorre los elementos del array de tabla
+    // y en cada vuelta se cakcule el price respecto al rate
+    console.log(event.id.substr(-1));
+    console.log('Array', this.selectedCarList[event.id.substr(-1)].current_price);
+
+
+    this.changeDR.detectChanges();
+    
+   
+
   }
+
+
+
 
 }
